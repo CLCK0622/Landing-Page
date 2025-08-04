670099195c0aa46a1bb054c5eeb607981119afbf
CLCK0622 <zhongyi070622@163.com>
2025-08-04 21:24:27 +0800

    feat: responsive design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,32 @@
+// app/layout.tsx
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
-import '../styles/globals.scss';
+import '@/styles/globals.scss';
+import styles from '@/styles/layout.module.scss';
 
 const roboto = Roboto({
-  weight: ['400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
+    weight: ['400', '500', '700'],
+    subsets: ['latin'],
+    display: 'swap',
 });
 
 export const metadata: Metadata = {
-  title: "Links - CLCKKKKK's Site",
-  description: 'My personal page.',
+    title: "CLCK's Site",
+    description: 'My personal page.',
 };
 
 export default function RootLayout({
-                                     children,
+                                       children,
                                    }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-      <html lang="en">
-      <body className={roboto.className}>{children}</body>
-      </html>
-  );
+    return (
+        <html lang="en">
+        <body className={roboto.className}>
+        <div className={styles.pageWrapper}>
+            {children}
+        </div>
+        </body>
+        </html>
+    );
 }
